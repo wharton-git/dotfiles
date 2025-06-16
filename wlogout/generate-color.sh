621ecb323089c46056215f4e3f3f3472caf2665f
Wharton Aldrick (Sanctuary) <whartonaldrick@gmail.com>
2025-06-17 01:10:44 +0300

    Remove generate-color script for wlogout

--- wlogout/generate-color.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fichiers
-BASE="$HOME/.config/wlogout/style-base.css"
-WAL="$HOME/.cache/wal/colors.css"
-FINAL="$HOME/.config/wlogout/style.css"
-
-# Concaténer les couleurs de wal en haut du fichier
-cat "$WAL" "$BASE" > "$FINAL"
